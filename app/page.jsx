4b4695b6bdc34bb70ccb952bcedc46c449f6a1c2
Clint Cloete <canoncollective38@gmail.com>
2025-02-03 00:23:40 +0200

    fix spelling error

--- app/page.jsx
+++ app/page.jsx
@@ -14,11 +14,10 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br /> <span className="text-accent">Clint Edward</span>
+              Hey, I'm <br /> <span className="text-accent">Clint Edward!</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experinces and quite
-              proficient i various programming languages and technologies.
+              I create digital experiences that are as smooth as your morning coffee. Whether designing intuitive interfaces or optimizing performance, I love bringing ideas to life with precision and creativity. My goal? Build solutions that just work—beautifully.
             </p>
 
             <div className="flex flex-col xl:flex-row items-center gap-8 ">
